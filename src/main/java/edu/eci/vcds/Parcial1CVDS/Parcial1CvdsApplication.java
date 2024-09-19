5af7c2b0453d3b98d0a5ae02c93281a148cc9443
JFM1028 <116533238+JFM1028@users.noreply.github.com>
2024-09-19 13:55:02 -0500

    creacion del proyecto ya con jacoco

---
+++ src/main/java/edu/eci/vcds/Parcial1CVDS/Parcial1CvdsApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.vcds.Parcial1CVDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial1CvdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial1CvdsApplication.class, args);
+	}
+
+}
